ae01d9c2940e405eeede2224f994a22b71d24fb9
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2021-12-18 18:06:12 +0200

    Create download-confluence.sh

---
+++ download-confluence.sh
@@ -0,0 +1,3 @@
+#wget https://www.atlassian.com/software/confluence/downloads/binary/tlassian-confluence-7.13.2.tar.gz
+wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.13.2.zip
+unzip atlassian-confluence-7.13.2.zip
