af51f844eb3ffd979941dab0252152de56edce49
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2021-12-19 17:30:56 +0200

    Update cloudflared-ubuntu.sh

--- cloudflared-ubuntu.sh
+++ cloudflared-ubuntu.sh
@@ -2,6 +2,8 @@
 #https://pkg.cloudflare.com/ 20.04 main' |
 #sudo tee /etc/apt/sources.list.d/cloudflare-main.list
 
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+
 echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/ focal main' |
 sudo tee /etc/apt/sources.list.d/cloudflare-main.list
 
