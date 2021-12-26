839e093c0015da2fe519e437119e63dce3a248b4
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2021-12-26 16:08:21 +0200

    Update microk8s.sh

--- kubernetes/microk8s.sh
+++ kubernetes/microk8s.sh
@@ -1,9 +1,26 @@
-sudo apt-get update
-sudo apt install snapd
+# sudo apt-get update
+# sudo apt install snapd
 
-sudo snap install microk8s --classic --channel=1.21
+# sudo snap install microk8s --classic --channel=1.21
+# sudo usermod -a -G microk8s $USER
+
+# sudo chown -f -R $USER ~/.kube
+# su - $USER
+# microk8s status --wait-ready
+
+yes | sudo apt-get update
+
+yes | sudo apt install snapd
+echo "snapd installed"
+
+yes | sudo snap install microk8s --classic
+echo "microk8s installed"
+
 sudo usermod -a -G microk8s $USER
-
 sudo chown -f -R $USER ~/.kube
+
 su - $USER
+echo "permissions configures"
 microk8s status --wait-ready
+
+microk8s enable dashboard dns ingress istio
