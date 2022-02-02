df2d59068a1a828bdc92ad0e9104bef90b777dbf
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-02-02 20:30:52 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -29,7 +29,6 @@ sudo usermod -aG docker $USER && newgrp docker
 #   }
 # }
 
-
 ### Docker-Compose
 echo "Installing Docker-Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
