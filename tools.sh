36bc3fd722569019bde4d980e536fbbe80f9ba3c
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-02-02 20:30:57 +0200

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -4,6 +4,14 @@ curl https://cli-assets.heroku.com/install.sh | sh
 
 # GCLOUD
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-359.0.0-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
+tar -xvzf google-cloud-sdk-359.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh -y
 ./google-cloud-sdk/bin/gcloud init
 
+# AWS
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# 
