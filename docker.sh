705f022949f67d9f71871bad96c245961e1da503
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-02-02 20:04:40 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 echo "Installing Docker..."
 yes | sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
