fbcb2712a7d740a1a50f2c645d1690c886ef612b
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-02-03 14:37:17 +0200

    Add files via upload

---
+++ atlas-b/sdk-deb.sh
@@ -0,0 +1,5 @@
+sudo sh -c 'echo "deb https://packages.atlassian.com/debian/atlassian-sdk-deb/ stable contrib" >>/etc/apt/sources.list'
+wget https://packages.atlassian.com/api/gpg/key/public    
+sudo apt-key add public
+sudo apt-get update
+sudo apt-get install atlassian-plugin-sdk
---
+++ atlas-b/sdk.sh
@@ -0,0 +1,4 @@
+# wget https://marketplace.atlassian.com/download/apps/1210993/version/42510
+wget https://marketplace.atlassian.com/download/apps/1210992/version/42510
+./sdk-installer-8.2.7.deb
+
