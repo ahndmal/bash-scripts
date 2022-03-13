1860324491c5f3a87eba7bd04e054f2cea858cc2
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-03-13 09:10:30 -0700

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -2,7 +2,9 @@ sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update -y
+echo "software successfully updated"
 sudo apt install caddy -y
+echo "Caddy installed!"
 
 
 #sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
