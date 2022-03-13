002ebdec28b91d2c5d9a5fc505e37bd9915cbd88
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-03-13 09:08:53 -0700

    Update sdkman.sh

--- sdkman.sh
+++ sdkman.sh
@@ -4,14 +4,17 @@ sudo apt install unzip -y
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
+echo "zip/unzip/SDK installed!"
 
 # java
 sdk i java 8.0.302-open -y
 sdk i java 11.0.12-open -y
 sdk i java 17.0.2-open -y
 
+# maven / gradle
 yes | sdk i maven 3.8.4
 sdk i gradle 7.3.3 -y
+echo "maven/gradle installed!"
 
 # groovy
 sdk i groovy 4.0.0 -y
