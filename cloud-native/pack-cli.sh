288e829659b207e9d7b207bfdda29738d73bbd16
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-04-22 17:47:22 +0300

    Update pack-cli.sh

--- cloud-native/pack-cli.sh
+++ cloud-native/pack-cli.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
-sudo apt-get update
-sudo apt-get install pack-cli
+sudo add-apt-repository ppa:cncf-buildpacks/pack-cli -y
+sudo apt-get update -y
+sudo apt-get install pack-cli -y
