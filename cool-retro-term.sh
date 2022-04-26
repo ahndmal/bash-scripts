77e0c2fe39ef5f1faf00cd106cbb72366520d8cc
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-04-26 14:33:03 +0300

    Create cool-retro-term.sh

---
+++ cool-retro-term.sh
@@ -0,0 +1,8 @@
+sudo apt install cool-retro-term -y
+
+#Appimage
+#A self-contained executable as AppImage is also available, which you can just download and run. No installation required. Follow the below commands to do that.
+
+wget https://github.com/Swordfish90/cool-retro-term/releases/download/1.1.1/Cool-Retro-Term-1.1.1-x86_64.AppImage
+chmod a+x Cool-Retro-Term-1.1.1-x86_64.AppImage
+./Cool-Retro-Term-1.1.1-x86_64.AppImage
