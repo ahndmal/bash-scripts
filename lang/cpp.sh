58723d320b709b91415a674d31e9e6cd0587b663
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-08-26 11:27:47 +0300

    Create cpp.sh

---
+++ lang/cpp.sh
@@ -0,0 +1,13 @@
+
+sudo apt update -y
+
+# Install the build-essential package by typing:
+sudo apt install build-essential
+
+# You may also want to install the manual pages about using GNU/Linux for development:
+sudo apt-get install manpages-dev
+
+
+
+# gcc FILE.c -o hello
+# ./FILE
