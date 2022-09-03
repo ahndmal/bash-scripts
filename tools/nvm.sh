1e59076713223efe8010e88cbdab33a52c9ab175
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-09-03 13:27:14 +0300

    Create nvm.sh

---
+++ tools/nvm.sh
@@ -0,0 +1,7 @@
+# https://github.com/nvm-sh/nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+nvm install node # "node" is an alias for the latest version
+#nvm install 14.7.0 # or 16.3.0, 12.22.1, etc
