9b091a5ac6cce2d79d905089b3974514c495d91d
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-09-03 13:10:02 +0300

    Update r.sh

--- lang/r.sh
+++ lang/r.sh
@@ -1,7 +1,7 @@
 # update indices
-sudo apt update -qq
+sudo apt update -qq -y
 # install two helper packages we need
-sudo apt install --no-install-recommends software-properties-common dirmngr
+sudo apt install --no-install-recommends software-properties-common dirmngr -y
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
@@ -16,10 +16,10 @@ sudo apt install --no-install-recommends r-base
 add-apt-repository ppa:c2d4u.team/c2d4u4.0+
 
 # add repos
-sudo apt-get install -y libxml2-dev libcurl4-openssl-dev libssl-dev
+sudo apt-get install -y libxml2-dev libcurl4-openssl-dev libssl-dev -y
 
-sudo apt install zypper
-sudo zypper install unixODBC-devel
+sudo apt install zypper -y
+sudo zypper install unixODBC-devel -y
 
-sudo apt install unixodbc-dev
-sudo apt-get install libpq-dev
+sudo apt install unixodbc-dev -y
+sudo apt-get install libpq-dev -y
