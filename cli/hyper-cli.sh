ee002fc7fab5b312eb9832e298bc74d66b0bb9f7
malandr <quadr988@gmail.com>
2022-09-20 18:35:31 +0300

    cli

---
+++ cli/hyper-cli.sh
@@ -0,0 +1,14 @@
+
+
+sudo apt install python3-pip -y
+
+pip install --user powerline-status
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+
+fc-cache -vf ~/.local/share/fonts/
+
+
---
+++ cli/oh_my_zsh.sh
@@ -0,0 +1,8 @@
+
+"https://github.com/ohmyzsh/ohmyzsh
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+"vi ~/.zshrc
+
+
