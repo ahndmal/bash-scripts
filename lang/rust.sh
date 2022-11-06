3d3cfa1b3d73244c166cde7b84b28a289f0d235f
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-11-06 13:57:18 +0200

    Update rust.sh

--- lang/rust.sh
+++ lang/rust.sh
@@ -10,3 +10,7 @@ rustup update
 
 # install openssl for build on Ubuntu
 sudo apt install libssl-dev
+
+# install Nightly builds for compatibility
+rustup install nightly
+cargo +nightly install racer
