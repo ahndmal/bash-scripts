419a3c719629e8ff5bb4c01ad4953d58fd5c7dc2
Andrii Maliuta <quadr988@gmail.com>
2022-12-03 17:54:32 +0100

    arch

--- ARCH/manjaro.sh
+++ ARCH/manjaro.sh
@@ -1,7 +1,9 @@
-# chrome
+# yay
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
+
+# yay - chrome
 yay -S google-chrome
 
 # make
@@ -9,9 +11,35 @@ sudo pacman -S make
 # neovim
 sudo pacman -Syu neovim
 
+# snap
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+
+sudo pacman -Syu zip unzip
+
 # docker
 sudo pacman -Syu
 sudo pacman -S docker
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# GO
+sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+#GTK / C
+sudo pacman -Sy base-devel
+
+#Java / sdkman
+curl -s "https://get.sdkman.io" | zsh
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+
+sdk i java 19-open
+sdk i java 11.0.12-open
+sdk i java 8.0.302-open
+sdk i java 22.3.r19-grl
