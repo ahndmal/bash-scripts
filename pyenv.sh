d12fb7286be3870b2950e5c4dd2343061a926ca3
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-12-17 12:05:36 +0100

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -2,3 +2,30 @@
 
 curl https://pyenv.run | bash
 
+# For bash:
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+#to add to ~/.profile:
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+#For Zsh:
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# install and use 3.10
+pyenv install 3.10
+pyenv global 3.10
+
+# To remove old Python versions, use ```pyenv uninstall <versions> ```
+
+## Update pyenv
+# pyenv update
+
+# remove
+# ```rm -rf $(pyenv root)```
