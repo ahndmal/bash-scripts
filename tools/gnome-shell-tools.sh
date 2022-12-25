9bfd1e0ce707df7ed037180f56e46b7f66ece451
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-12-25 15:49:45 +0200

    Create gnome-shell-tools.sh

---
+++ tools/gnome-shell-tools.sh
@@ -0,0 +1,4 @@
+# https://wiki.gnome.org/action/show/Projects/GnomeShellIntegration/Installation?action=show&redirect=Projects%2FGnomeShellIntegrationForChrome%2FInstallation
+
+sudo apt-get install chrome-gnome-shell
+
