e3471162e42ccccc07c734e3d15aa0d10ae62a56
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2023-01-08 11:48:35 +0200

    Create wasm-cloud.sh

---
+++ WASM/wasm-cloud.sh
@@ -0,0 +1,7 @@
+#https://wasmcloud.dev/overview/installation/install-with-docker/
+
+wget https://raw.githubusercontent.com/wasmCloud/examples/main/docker/docker-compose.yml
+
+docker compose up
+
+docker logs -f wasmcloud
