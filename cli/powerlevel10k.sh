ca06b41c4a2036ef488e32411cda2b9991eb2fde
Andrii Maliuta <quadr988@gmail.com>
2023-01-22 17:30:02 +0200

    Create powerlevel10k.sh

---
+++ cli/powerlevel10k.sh
@@ -0,0 +1,5 @@
+# https://github.com/romkatv/powerlevel10k#oh-my-zsh
+
+######### powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
