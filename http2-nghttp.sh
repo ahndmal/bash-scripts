62063a11e09d2156b69bc57d246e0404e15e8fe6
Andrii Maliuta <quadr988@gmail.com>
2023-01-26 19:43:01 +0200

    Create http2-nghttp.sh

---
+++ http2-nghttp.sh
@@ -0,0 +1,9 @@
+
+# https://nghttp2.org/documentation/
+
+sudo apt-get install g++ clang make binutils autoconf automake \
+  autotools-dev libtool pkg-config \
+  zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev \
+  libevent-dev libjansson-dev \
+  libc-ares-dev libjemalloc-dev libsystemd-dev \
+  ruby-dev bison libelf-dev
