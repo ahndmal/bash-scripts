832c5fecba1f37608e17d01ee3eb55b3689c0463
Andrii Maliuta <quadr988@gmail.com>
2023-01-29 20:24:30 +0200

    net

---
+++ net/http-tools.sh
@@ -0,0 +1,14 @@
+
+sudo apt install build-essential meson libncurses6-dev
+
+sudo apt install -y curl vnstat nmap net-tools
+sudo apt install nghttp2
+
+
+sudo apt update
+sudo apt install snapd
+
+snap install httpie
+
+
+
