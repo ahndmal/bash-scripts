e00d05a4d4174a1ae962f88e6f108be1d8eea7ac
Andrii Maliuta <quadr988@gmail.com>
2023-01-29 17:47:23 +0200

    Update configure_zsh.sh

--- cli/configure_zsh.sh
+++ cli/configure_zsh.sh
@@ -1,1 +1,27 @@
-TODO
+
+
+
+# oh my ash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
+zsh
+fish
+
+# fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+fisher install jorgebucaran/nvm.fish
+fisher install IlanCosman/tide@v5
+fisher install jethrokuan/z
+# https://github.com/craftzdog/dotfiles-public
+
+
+## devicons for Nvim
+#https://github.com/ryanoasis/vim-devicons
+# Plug 'ryanoasis/vim-devicons'
+
