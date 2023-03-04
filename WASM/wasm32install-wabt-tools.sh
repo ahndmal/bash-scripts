1e4e133fe45a14f117b3db013f4af0660791bea6
Andrii Maliuta <quadr988@gmail.com>
2023-03-04 20:28:16 +0200

    Update wasm32install-wabt-tools.sh

--- WASM/wasm32install-wabt-tools.sh
+++ WASM/wasm32install-wabt-tools.sh
@@ -5,7 +5,10 @@ cd wabt
 git submodule update --init
 
 # install cmake (pacman / apt)
+wget https://github.com/Kitware/CMake/releases/download/v3.26.0-rc5/cmake-3.26.0-rc5.tar.gz
+tar -xvf cmake-3.26.0-rc5.tar.gz
 
+# build
 mkdir build
 cd build
 cmake ..
