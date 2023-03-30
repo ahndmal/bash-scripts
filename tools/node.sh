24c025246ab2cf35eb44d643f0d1315ca9294457
Andrii Maliuta <quadr988@gmail.com>
2023-03-30 20:47:12 +0300

    Delete node.sh

--- tools/node.sh
+++
@@ -1,7 +0,0 @@
-# Node from https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# NPM
-sudo apt intall npm -y
-#apt-get install -y build-essential
