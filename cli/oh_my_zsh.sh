f9cbe2b085387756ffeca646968c7dcd69783c4f
Andrii Maliuta <quadr988@gmail.com>
2023-05-30 20:35:07 +0300

    Update oh_my_zsh.sh

--- cli/oh_my_zsh.sh
+++ cli/oh_my_zsh.sh
@@ -1,8 +1,8 @@
-
+### oh_my_zsh
 "https://github.com/ohmyzsh/ohmyzsh
-
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 "vi ~/.zshrc
 
+### powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
