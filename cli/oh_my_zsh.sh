3bf649b8ea633af1a9e8969b3e96afb5de8c7da9
Andrii Maliuta <quadr988@gmail.com>
2023-05-30 20:42:19 +0300

    Update oh_my_zsh.sh

--- cli/oh_my_zsh.sh
+++ cli/oh_my_zsh.sh
@@ -6,3 +6,15 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 ### powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
+# OZSH plugins
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# npm install -g secman
+
+## Colors
+
+### exa (https://github.com/ogham/exa)
+# wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip
+sudo apt install exa
