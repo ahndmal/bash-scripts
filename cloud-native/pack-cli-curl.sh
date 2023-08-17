f416d6704e8533c0170b22217b38a380864a4925
Andrii Maliuta <quadr988@gmail.com>
2023-08-17 19:49:24 +0300

    Update pack-cli-curl.sh

--- cloud-native/pack-cli-curl.sh
+++ cloud-native/pack-cli-curl.sh
@@ -1,1 +1,1 @@
-curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.24.0/pack-v0.24.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
+curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.24.0/pack-v0.24.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack
