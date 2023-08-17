57a879285bbf4ef739286db99f6f9941148a29a5
Andrii Maliuta <quadr988@gmail.com>
2023-08-17 20:04:53 +0300

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -1,6 +1,6 @@
-# Heroku
-#sudo snap install --classic heroku
-curl https://cli-assets.heroku.com/install.sh | sh
+##  Heroku
+# sudo snap install --classic heroku
+# curl https://cli-assets.heroku.com/install.sh | sh
 
 # GCLOUD
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-359.0.0-linux-x86_64.tar.gz
