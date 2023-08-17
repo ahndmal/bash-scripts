af134e7661bd8e2cdfc04b988aebf9dcd9fb9fbe
Andrii Maliuta <quadr988@gmail.com>
2023-08-17 20:10:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,29 @@
-echo ">>>> configuring tools"
+echo ">>>> \u001b[36mconfiguring tools\u001b[0m"
 
 source atlas-b/sdk.sh
 
 source cli/configureZSH.sh
 
-echo ">>> installing Cloud tools"
+echo ">>> \u001b[36minstalling Cloud tools\u001b[0m"
 source cloud-native/pack=cli.sh
 
-source frame/helidon=cli.sh
+source frame/helidon-cli.sh
 
 source tools/sdkman.sh
+source tools/pyenv.sh
+source tools/nvm_nodejs.sh
 
-echo ">>> installing PLs"
+echo "\u001b[35m>>> installing PLs\u001b[0m"
 source lang/cpp.sh
 source lang/lua.sh
 source lang/rust.sh
 
-echo ">>> installing HTTP tools"
+echo ">>> installing HTTP tools\u001b[0m"
 
 source net/http-tools.sh
 
 source docker.sh
 source fira-code-font.sh
+
+echo "#######################################################"
+echo ">>>> \u001b[32mFinished installation!\u001b[0m"
