5036df36d328d6a65cd9f1a427c31476942e9ff8
Andrii Maliuta <quadr988@gmail.com>
2023-10-21 15:07:13 +0300

    Create wasm_tools.sh

---
+++ WASM/wasm_tools.sh
@@ -0,0 +1,13 @@
+
+
+## wasmtime (https://github.com/bytecodealliance/wasmtime)
+curl https://wasmtime.dev/install.sh -sSf | bash
+
+## wasmedge (https://wasmedge.org/docs/start/install/)
+curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash
+source $HOME/.wasmedge/env
+
+# curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | sudo bash -s -- -p /usr/local
+
+## 
+
