9bf48d08c0b3fb353c5a69127e94126cfc942a65
Andrii Maliuta <quadr988@gmail.com>
2023-10-22 14:56:01 +0300

    Update wasm_tools.sh

--- WASM/wasm_tools.sh
+++ WASM/wasm_tools.sh
@@ -25,3 +25,28 @@ cd build
 cmake ..
 cmake --build .
 
+############################# EMSDK (https://emscripten.org/docs/getting_started/downloads.html)
+
+# Get the emsdk repo
+git clone https://github.com/emscripten-core/emsdk.git
+
+# Enter that directory
+cd emsdk
+
+# Fetch the latest version of the emsdk (not needed the first time you clone)
+git pull
+
+# Download and install the latest SDK tools.
+./emsdk install latest
+
+# Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+./emsdk activate latest
+
+# Activate PATH and other environment variables in the current terminal
+source ./emsdk_env.sh
+
+# ./emsdk install 1.38.45
+./emsdk install latest
+
+
+
