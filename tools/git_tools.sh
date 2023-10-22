35646b33a905c9ce9ae7e1323925f8ebc3bee80c
Andrii Maliuta <quadr988@gmail.com>
2023-10-22 11:15:28 +0300

    Create git_tools.sh

---
+++ tools/git_tools.sh
@@ -0,0 +1,16 @@
+
+########## git
+sudo apt install git
+
+########## gh (https://github.com/cli/cli/blob/trunk/docs/install_linux.md)
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+
+git config --global user.name "Andrii Maliuta" 
+git config --global user.email quadr988@gmail.com
+
