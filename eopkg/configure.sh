04b610897b06548126ba628b88f7321aec7593d4
Andrii Maliuta <quadr988@gmail.com>
2024-06-01 17:49:36 +0300

    Update configure.sh

--- eopkg/configure.sh
+++ eopkg/configure.sh
@@ -1,7 +1,9 @@
+# https://help.getsol.us/docs/user/package-management/basics/#updating
 
 sudo eopkg upgrade
 
-sudo eopkg install tmux neovim fish
+sudo eopkg install tmux neovim zip unzip git exa 
+
 sudo eopkg it -c system.devel
 
 sudo eopkg it -c docker
