becfc767165821530ed3cbdf152d6288fd5a1b00
Andrii Maliuta <quadr988@gmail.com>
2024-06-20 20:31:36 +0300

    Update and rename fira-code-font.sh to install-fonts.sh

--- fira-code-font.sh
+++
@@ -1,7 +0,0 @@
-# FIra
-# https://github.com/panr/hugo-theme-terminal
-
-wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
-
-sudo apt install -y fonts-firacode
-
---
+++ install-fonts.sh
@@ -0,0 +1,15 @@
+############  Fira code
+## https://github.com/panr/hugo-theme-terminal
+
+echo ">>> "
+
+wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+
+sudo apt install -y fonts-firacode
+
+############## Powerline
+## https://github.com/powerline/fonts?tab=readme-ov-file
+## https://powerline.readthedocs.io/en/latest/installation/linux.html#fonts-installation
+
+sudo apt-get install fonts-powerline
+
