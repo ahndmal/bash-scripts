efaa1544baf4b82c05d35a07995f65c52df26ba5
Andrii Maliuta <quadr988@gmail.com>
2024-06-22 19:33:45 +0300

    Update CONFIGURE_UBUNTU.sh

--- CONFIGURE_UBUNTU.sh
+++ CONFIGURE_UBUNTU.sh
@@ -1,15 +1,29 @@
 echo "==== starting installations..."
 echo date +"%d/%m/%Y - %H:%M:%S"
 
+sudo apt-get update -y
+
+sudo apt-get install zsh tmux zip unzip neovim git fuse
+
+echo ">>>> Installed main tools \n"
+
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 
-curl https://github.com/AndriiMaliuta/bash-scripts
+mkdir tools && cd tools
+
+echo ">>> Installing my bash scripts from GIT \n"
+
+git clone https://github.com/AndriiMaliuta/bash-scripts
 
 chmod -R +x .
 
-sudo apt-get install build-essential libz-dev zlib1g-dev
+sudo apt-get install build-essential libz-dev zlib1g-dev -y
 
-chmod -R +x .
+### Helix
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
+
 
 ./cli/configure_ZSH.sh 
 ./tools/sdkman.sh
