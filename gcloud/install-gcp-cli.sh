3cf102b7e7308c1bb93bff18a98f414d0288a35f
Andrii Maliuta <quadr988@gmail.com>
2024-06-22 20:03:00 +0300

    Create install-gcp-cli.sh

---
+++ gcloud/install-gcp-cli.sh
@@ -0,0 +1,5 @@
+### https://cloud.google.com/sdk/docs/install
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-481.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-481.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
