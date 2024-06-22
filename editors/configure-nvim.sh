a9921e3cdebd59638ea51f3a5dcf6a5aec4e93eb
Andrii Maliuta <quadr988@gmail.com>
2024-06-22 19:38:51 +0300

    Update configure-nvim.sh

--- editors/configure-nvim.sh
+++ editors/configure-nvim.sh
@@ -4,7 +4,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # get init.vim example file
-curl https://github.com/ahndmal/.dotfiles/raw/main/.config/nvim/init.vim
+wget https://github.com/ahndmal/.dotfiles/raw/main/.config/nvim/init.vim
 
 mkdir ~/.config/nvim
 
