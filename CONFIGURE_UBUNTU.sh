c67bffdb42ed4878faaa525773ee68e9931248f5
Andrii Maliuta <quadr988@gmail.com>
2024-06-22 19:56:44 +0300

    Update CONFIGURE_UBUNTU.sh

--- CONFIGURE_UBUNTU.sh
+++ CONFIGURE_UBUNTU.sh
@@ -24,7 +24,27 @@ sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
 
+wget https://github.com/ahndmal/.dotfiles/raw/main/helix/config.toml
+wget https://github.com/ahndmal/.dotfiles/raw/main/helix/languages.toml
 
+mv *.toml ~/.config/helix
+
+#############################
+./install-fonts.sh
 ./cli/configure_ZSH.sh 
 ./tools/sdkman.sh
 
+####################### Java
+
+sdk i java 22.0.1-graalce
+sdk i java 22-open
+sdk i java 17.0.11-tem
+sdk i java 11.0.23-tem
+sdk i java 8.0.412-tem
+
+####################### GO
+# https://go.dev/doc/install
+# https://go.dev/dl/
+
+wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+tar -xvf go1.22.4.linux-amd64.tar.gz
