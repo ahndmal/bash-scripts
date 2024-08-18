2d8b9e24fe3de85f7705efeb0ef1d2ba43874cfd
Andrii Maliuta <quadr988@gmail.com>
2024-08-18 11:52:31 +0000

    Update configure_ZSH.sh

--- cli/configure_ZSH.sh
+++ cli/configure_ZSH.sh
@@ -18,17 +18,17 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 ### Colors
 
-## Install EXA (https://github.com/ogham/exa) 
-# https://the.exa.website/
+## Install EZA (https://github.com/eza-community/eza/blob/main/INSTALL.md) 
 
-EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
-sudo unzip -q exa.zip bin/exa -d /usr/local
-exa --version
-rm -rf exa.zip
-exa /etc -lhgi
+sudo apt update
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
 
-# sudo apt install exa
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 echo ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc
 echo "plugins=( git zsh-syntax-highlighting zsh-autosuggestions )" >> ~/.zshrc
