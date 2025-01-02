876022496384f580dba3584387bba8f1bcddd5ea
Andrii Maliuta <quadr988@gmail.com>
2025-01-02 15:27:12 +0300

    Create install_env.sh

---
+++ cli/opensuse/install_env.sh
@@ -0,0 +1,18 @@
+
+
+
+### TOOLS
+sudo zypper in vim neovim helix git
+
+### FISH (https://software.opensuse.org/download.html?project=shells%3Afish%3Arelease%3A3&package=fish)
+# For 15.6 run the following as root:
+
+sudo zypper addrepo https://download.opensuse.org/repositories/shells:fish:release:3/15.6/shells:fish:release:3.repo
+sudo zypper refresh
+sudo zypper install fish
+
+
+# EZA (https://github.com/eza-community/eza/blob/main/INSTALL.md)
+zypper ar https://download.opensuse.org/tumbleweed/repo/oss/ factory-oss
+zypper in eza
+
