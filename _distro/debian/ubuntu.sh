f6ae206e126485b04cc536bf8d0a32945fded9b8
Andrii Maliuta <quadr988@gmail.com>
2025-09-21 10:02:06 +0300

    Update ubuntu.sh

--- _distro/debian/ubuntu.sh
+++ _distro/debian/ubuntu.sh
@@ -1,4 +1,6 @@
 
 sudo apt-get update -y
 
-sudo apt-get install zsh tmux zip unzip neovim emacs git fuse cmake 
+sudo apt-get install zsh tmux zip unzip neovim emacs git fuse 
+
+sudo apt-get install libgtest-dev cmake
