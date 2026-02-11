91b5bf8e9dafa14fb4dcdffe20946dedb92fd207
Andrii Maliuta <quadr988@gmail.com>
2026-02-11 21:42:03 +0200

    Update wasm_tools.sh

--- wasm/wasm_tools.sh
+++ wasm/wasm_tools.sh
@@ -1,14 +1,28 @@
 
-#################### wasmtime (https://github.com/bytecodealliance/wasmtime)
+#################### Wasmtime (https://github.com/bytecodealliance/wasmtime)
+
 curl https://wasmtime.dev/install.sh -sSf | bash
 
-#################### wasmedge (https://wasmedge.org/docs/start/install/)
+#################### Wasmedge (https://wasmedge.org/docs/start/install/)
+
 curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash
 source $HOME/.wasmedge/env
 
 # curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | sudo bash -s -- -p /usr/local
 
-#################### WABT ( https://github.com/webassembly/wabt )
+################### Wasm PACK
+
+cargo install wasm-pack
+
+############# Cargo Component
+
+cargo install --locked cargo-component
+
+#################### WASM TOOLS (https://github.com/bytecodealliance/wasm-tools)
+
+cargo install --locked wasm-tools
+
+#################### WABT TOOLS ( https://github.com/webassembly/wabt )
 
 git clone --recursive https://github.com/WebAssembly/wabt
 cd wabt
